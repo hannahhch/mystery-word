88ef2343ab17400980d34aec26247116e3f248a2
Hannah Hudson <hannah.hch@gmail.com>
2017-08-23 14:22:38 -0400

    dictionary words connected and consoling

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const mustache = require('mustache-express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const parseurl = require('parseurl');
-
+const fs = require('fs');
+//set words into a variable to access dict/words file from computer and split
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 //create the express app
 const app = express();
+
+console.log(words);
 
 //set an arrary for the guessed letters
 let guessArr = [];
@@ -32,11 +36,12 @@ app.use(session({
   saveUninitialized: true
 }));
 
-//start by rendering mustache to page
+//start by rendering mustache to page with the array for guessed letters
 app.get('/', function(req, res){
   res.render('index', { guessArr:guessArr });
 });
 
+//each time a guess is made, the letters will be pushed onto the array and posted to the request body.
 app.post('/', function(req, res){
   guessArr.push(req.body.guessBox);
   res.redirect('/');
